05eb999e4f0da169176e9ddab95a58c555da6075
Jon Higger <jonathan.higger@gmail.com>
2025-01-14 18:13:24 -0700

    got rid of dotenv and added wrangler

--- dist/_worker.js/index.js
+++ dist/_worker.js/index.js
@@ -1,2 +1,2 @@
-globalThis._importMeta_={url:"file:///_entry.js",env:{}};import"node:buffer";export{l as default}from"./chunks/nitro/nitro.mjs";import"node:path";import"node:crypto";import"node:async_hooks";import"node:stream";
+globalThis._importMeta_={url:"file:///_entry.js",env:{}};import"node:buffer";export{k as default}from"./chunks/nitro/nitro.mjs";import"node:async_hooks";import"node:stream";
 //# sourceMappingURL=index.js.map
