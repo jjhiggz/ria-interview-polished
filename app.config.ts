86dbc878780f8276f4549e8da52466931c942daa
Jon Higger <jonathan.higger@gmail.com>
2025-01-14 18:02:04 -0700

    should work now with cloudflare

--- app.config.ts
+++ app.config.ts
@@ -1,12 +1,17 @@
-import { defineConfig } from '@tanstack/start/config'
-import tsConfigPaths from 'vite-tsconfig-paths'
+import { defineConfig } from "@tanstack/start/config";
+import { cloudflare } from "unenv";
+import tsConfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
   vite: {
     plugins: [
       tsConfigPaths({
-        projects: ['./tsconfig.json'],
+        projects: ["./tsconfig.json"],
       }),
     ],
   },
-})
+  server: {
+    preset: "cloudflare-pages",
+    unenv: cloudflare,
+  },
+});
